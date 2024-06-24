8d6c50de87064fb8d8d93baa92e14e0509488e34
shahid <shahidazhari313@gmail.com>
2024-06-24 21:36:49 +0530

    logout ui fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,40 @@
 import { useAuth } from "../hooks/useAuth";
+import headImg from '../assets/img_header_logo.png'; // Assuming you have the image path correctly defined
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate()
 
-  const { user } = useAuth()
-  console.log(user);
-
+  const handleLogout = async () => {
+    await logout();
+    navigate('/sign-in');
+  };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full sm:max-w-md">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">Welcome </h1>
-        <div className="mb-4">
-          <p className="text-xl text-blue-600 font-semibold mb-2">{user?.email}</p>
-          <p className="text-lg text-gray-700">{user?.phoneNumber}</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="w-full flex justify-end pr-8">
+        <img src={headImg} alt="Header Logo" className="w-20 h-auto mb-4" />
+      </div>
+
+      <div className="bg-white p-8 rounded-lg shadow-md w-full sm:max-w-md flex flex-col items-center justify-center">
+        <div className="w-full max-w-6xl flex flex-col items-center justify-center px-4 mb-8">
+          <img src={headImg} alt="Header Logo" className="w-8 h-auto mb-4" />
+          <h1 className="text-3xl font-bold text-gray-800 mb-2 text-center">Welcome</h1>
+          <div className="mb-2 text-center">
+            <p className="text-xl text-blue-600 font-semibold">{user?.email}</p>
+            <p className="text-xl text-blue-500 font-semibold">{user?.phoneNumber}</p>
+          </div>
+        </div>
+
+        <div className="mt-6">
+          <button
+            onClick={handleLogout}
+            className=" w-60 px-4 py-2 bg-blue-500 text-white rounded-md font-semibold hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"
+          >
+            Logout
+          </button>
+
         </div>
       </div>
     </div>
