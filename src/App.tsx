93012f74e555cafa1bd85e832a662143f2d747a2
shahid <shahidazhari313@gmail.com>
2024-06-24 11:23:24 +0530

    last

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={user ? <Home /> : <Navigate to={'/sign-in'} />} />
 
+          <Route path='/sign-in' element={!user ? <SignIn />: <Navigate to={'/'} />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/about' element={<About />} />
         </Routes>
